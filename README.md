0428908fdc4493606bbca3ad5bc18f4e949cf00d
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-09-10 13:42:18 -0500

    Created new file, added placeholder code
    Added .wav file specifications and added a code example to the voice-program.py file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Voice Project
  My Capstone Project - covers fluency of speakers
+
+Audio files must be in *.wav format, recorded at 44 kHz sample frame and 16 bits of resolution.
---
+++ voice-program.py
@@ -0,0 +1,4 @@
+mysp=__import__("my-voice-analysis")
+
+#p="Walkers"  Audio File title
+#c=r"C:\Users\Rene Ramirez\Desktop\Capstone Things\Voice Recordings" Path to the Audio_File directory (Python 3.7)
