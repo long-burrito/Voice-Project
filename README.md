659cddd58ddbcc869db644fcb2a47cc6d345d8b6
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-09-10 16:13:25 -0500

    Added test samples, resolved main issue
    Recorded a few test samples to test python program, maybe not the most accurate thing.
    
    Found out the source of the error. The solution to the audio not being understood was simply to move next to the camera, put it in mono audio, and speak louder.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
  My Capstone Project - covers fluency of speakers
 
 Audio files must be in *.wav format, recorded at 44 kHz sample frame and 16 bits of resolution.
+YOU MUST SPEAK RIGHT NEXT TO THE MICROPHONE, OTHERWISE THIS DOESN"T WORK
--- voice-program.py
+++ voice-program.py
@@ -1,4 +1,11 @@
 mysp=__import__("my-voice-analysis")
 
+
 #p="Walkers"  Audio File title
 #c=r"C:\Users\Rene Ramirez\Desktop\Capstone Things\Voice Recordings" Path to the Audio_File directory (Python 3.7)
+
+p="banana"
+c=r"C:\Users\Rene Ramirez\Documents\GitHub\Voice-Project\Voice Recordings"
+
+
+mysp.mysptotal(p,c)
