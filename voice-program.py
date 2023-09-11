63b155bb60e6c05462acb66d668b6cbc48061377
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-09-11 10:47:27 -0500

    Update voice-program.py
    There is a bug to where "parselmouth.PraatError: Cannot open file “C:\Users\rener\Documents\GitHub\Voice-Project\myprosody\dataset\audioFiles\dataset\essen\MLTRNL.praat”.
    Error reading file “C:\Users\rener\Documents\GitHub\Voice-Project\myprosody\dataset\audioFiles\dataset\essen\MLTRNL.praat”." But,

--- voice-program.py
+++ voice-program.py
@@ -1,11 +1,11 @@
-mysp=__import__("my-voice-analysis")
-
+mysp=__import__("myprosody")
+import pickle
 
 #p="Walkers"  Audio File title
 #c=r"C:\Users\Rene Ramirez\Desktop\Capstone Things\Voice Recordings" Path to the Audio_File directory (Python 3.7)
 
-p="banana"
-c=r"C:\Users\Rene Ramirez\Documents\GitHub\Voice-Project\Voice Recordings"
+p="interact names"
+c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody\dataset\audioFiles"
 
 
-mysp.mysptotal(p,c)
+mysp.myprosody(p,c)
