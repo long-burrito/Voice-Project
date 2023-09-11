61142bdff317f8cff73e73a5160f6474c9c2c319
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-09-11 11:10:41 -0500

    Bug Fix and Upload Files
    Fixed the bug, apparently I had to link to the prosody folder, not the actual audio folder. Also, uploaded some TOEFL practice paragraphs to push the program to its limit. I will read them later.

--- voice-program.py
+++ voice-program.py
@@ -2,10 +2,10 @@ mysp=__import__("myprosody")
 import pickle
 
 #p="Walkers"  Audio File title
-#c=r"C:\Users\Rene Ramirez\Desktop\Capstone Things\Voice Recordings" Path to the Audio_File directory (Python 3.7)
+#c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody" Path to the Prosody folder (Python 3.7)
 
 p="interact names"
-c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody\dataset\audioFiles"
+c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody"
 
 
 mysp.myprosody(p,c)
