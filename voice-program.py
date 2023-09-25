8fbd6197310ab5a6d782ffac63652c391feb34ec
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-09-25 10:45:36 -0500

    Update voice-program.py

--- voice-program.py
+++ voice-program.py
@@ -1,19 +1,10 @@
-mysp=__import__("myprosody")
+mysp = __import__("myprosody")
 import pickle
 
 #p="Walkers"  Audio File title
 #c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody" Path to the Prosody folder (Python 3.7)
 
-p="interact names"
+p="banana"
 c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody"
 
-
-mysp.myspgend(p,c)
-
-p="banana"
-
-mysp.myspgend(p,c)
-
-p="mad"
-
-mysp.myspgend(p,c)
+mysp.myspsyl(p,c)
