2df95ad6cad856d74c893350fbc56445927d8d03
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-09-25 11:17:41 -0500

    New testing
    testing syllable count and speed

--- voice-program.py
+++ voice-program.py
@@ -4,7 +4,20 @@ import pickle
 #p="Walkers"  Audio File title
 #c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody" Path to the Prosody folder (Python 3.7)
 
-p="banana"
+p="PPAP"
 c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody"
 
 mysp.myspsyl(p,c)
+
+p="passage 2 slow"
+mysp.myspatc(p,c)
+
+p="passage 2 normal"
+mysp.myspatc(p,c)
+
+p="passage 2 fast"
+mysp.myspatc(p,c)
+
+p="fast 2 channel"
+mysp.myspatc(p,c)
+
