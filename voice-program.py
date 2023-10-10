c4e7038ca2e9571901e6aeb85a95983fbd12333e
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-10-10 10:02:29 -0500

    Update voice-program.py

--- voice-program.py
+++ voice-program.py
@@ -8,16 +8,20 @@ p="PPAP"
 c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody"
 
 mysp.myspsyl(p,c)
+mysp.myspgend(p,c)
 
 p="passage 2 slow"
 mysp.myspatc(p,c)
+mysp.myspgend(p,c)
 
 p="passage 2 normal"
 mysp.myspatc(p,c)
+mysp.myspgend(p,c)
 
 p="passage 2 fast"
 mysp.myspatc(p,c)
+mysp.myspgend(p,c)
 
 p="fast 2 channel"
 mysp.myspatc(p,c)
-
+mysp.myspgend(p,c)
