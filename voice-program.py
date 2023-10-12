09150552a7c325c4825cf85286ef844e23d77a14
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-10-12 10:20:24 -0500

    test runs

--- voice-program.py
+++ voice-program.py
@@ -4,24 +4,9 @@ import pickle
 #p="Walkers"  Audio File title
 #c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody" Path to the Prosody folder (Python 3.7)
 
-p="PPAP"
+p="passage 1"
 c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody"
 
-mysp.myspsyl(p,c)
-mysp.myspgend(p,c)
+mysp.myprosody(p,c)
 
-p="passage 2 slow"
-mysp.myspatc(p,c)
-mysp.myspgend(p,c)
-
-p="passage 2 normal"
-mysp.myspatc(p,c)
-mysp.myspgend(p,c)
-
-p="passage 2 fast"
-mysp.myspatc(p,c)
-mysp.myspgend(p,c)
-
-p="fast 2 channel"
-mysp.myspatc(p,c)
-mysp.myspgend(p,c)
+mysp.mysplev(p,c)
