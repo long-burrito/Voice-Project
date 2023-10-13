c9c1016f38767e688e9067b04d44bdc899110769
long_burrito <101278892+long-burrito@users.noreply.github.com>
2023-10-13 09:38:50 -0500

    Uploaded new docs
    Added new information, ran some prosody tests and encountered an error

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Voice Project
 My Capstone Project - covers fluency of speakers
 
-Audio files must be in *.wav format, recorded at 48 kHz sample frame and 24-32 bits of resolution.
+Audio files must be in *.wav format, recorded at 48 kHz sample frame and 24-32 bits of resolution. Record in MONO
 YOU MUST SPEAK RIGHT NEXT TO THE MICROPHONE, OTHERWISE THIS DOESN"T WORK
 
 Audacity is a good tool to use for recording at the exact specifications.
--- voice-program.py
+++ voice-program.py
@@ -1,12 +1,16 @@
-mysp = __import__("myprosody")
+import myprosody as mysp
 import pickle
 
 #p="Walkers"  Audio File title
 #c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody" Path to the Prosody folder (Python 3.7)
 
-p="passage 1"
+p="french"
 c=r"C:\Users\rener\Documents\GitHub\Voice-Project\myprosody"
 
 mysp.myprosody(p,c)
-
+mysp.myspsyl(p,c)
+mysp.mysppaus(p,c)
 mysp.mysplev(p,c)
+
+#ratio = syllable_count/pauses
+#print(f"The ratio between syllable count and pauses is {ratio}")
